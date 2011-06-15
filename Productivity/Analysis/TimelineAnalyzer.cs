34b45a661b9a4f679b19fc4e78ae3fb33a4838cb
John Gietzen <otac0n@gietzen.us>
2011-06-15 22:02:47 +0000

    Added timeline analyzer.

---
+++ Productivity/Analysis/TimelineAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿namespace Productivity.Analysis
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Productivity.Models;
+
+    public class TimelineAnalyzer
+    {
+        private EventsConnection db;
+
+        public TimelineAnalyzer(EventsConnection db)
+        {
+            this.db = db;
+        }
+
+        internal void Analyze(DateTime startTime, DateTime endTime)
+        {
+            var events = (from e in this.db.Events
+                          where e.EndTime >= startTime
+                          where e.StartTime <= endTime
+                          select e).ToList();
+
+            var times = new HashSet<DateTime>();
+            foreach (var e in events)
+            {
+                times.Add(e.StartTime);
+                times.Add(e.EndTime);
+            }
+        }
+    }
+}
--- Productivity/ProductivityView.cs
+++ Productivity/ProductivityView.cs
@@ -1,19 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-
-namespace Productivity
+﻿namespace Productivity
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Forms;
+    using Productivity.Models;
+    using Productivity.Analysis;
+
     public partial class ProductivityView : Form
     {
+        private EventsConnection db;
+
         public ProductivityView()
         {
+            this.db = new EventsConnection();
             InitializeComponent();
+
+            var timelineAnalyzer = new TimelineAnalyzer(this.db);
+            var startTime = DateTime.UtcNow.Date;
+            timelineAnalyzer.Analyze(startTime, startTime.AddDays(1));
         }
 
         private void ProductivityView_FormClosing(object sender, FormClosingEventArgs e)
