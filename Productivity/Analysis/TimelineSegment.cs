d93f56500e21d8537f36c3ada45e4c7683061d09
John Gietzen <otac0n@gietzen.us>
2011-09-07 17:08:18 -0500

    Added a `TimelineSegment` class for holding the details of a specific time.

---
+++ Productivity/Analysis/TimelineSegment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Productivity.Analysis
+{
+    internal class TimelineSegment
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; }
+        public int Productivity { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1} ({2}%): {3}", this.StartTime, this.EndTime, this.Productivity, this.Description);
+        }
+    }
+}
